7937554a352b7d3ed06a7384a33b9399e1224087
kemokemo <t2wonderland@gmail.com>
2020-03-03 00:16:36 +0900

    refactor: add main function. it's good.

--- tiny-hello/hello.py
+++ tiny-hello/hello.py
@@ -10,6 +10,10 @@ def test_greet():
     assert greet('World') == 'Hello, World!'
     assert greet('KemoKemo') == 'Hello, KemoKemo!'
 
-args = sys.argv[1:]
-name = args[0] if args else 'World'
-print(greet(name))
+def main():
+    args = sys.argv[1:]
+    name = args[0] if args else 'World'
+    print(greet(name))
+
+if __name__ == '__main__':
+    main()
