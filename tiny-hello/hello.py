5db73d6d0cd81fa9a2d91ba79a3ca8b6c8672d19
kemokemo <t2wonderland@gmail.com>
2020-03-02 12:58:44 +0900

    feat: add tiny hello.

---
+++ tiny-hello/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Purpose: Say hello
+
+import sys
+
+def greet(name):
+    return f'Hello, {name}!'
+
+def test_greet():
+    assert greet('World') == 'Hello, World!'
+    assert greet('KemoKemo') == 'Hello, KemoKemo!'
+
+args = sys.argv[1:]
+name = args[0] if args else 'World'
+print(greet(name))
