fcc3a9d572e71164fc9ee1600aaa6121b601edc9
kemokemo <t2wonderland@gmail.com>
2020-03-03 13:02:58 +0900

    feat: add type and usage.

--- tiny-hello/hello.py
+++ tiny-hello/hello.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 # Purpose: Say hello
 
+import os
 import sys
 
-def greet(name):
+def greet(name: str) -> str:
     return f'Hello, {name}!'
 
-def test_greet():
+def test_greet() -> None:
     assert greet('World') == 'Hello, World!'
     assert greet('KemoKemo') == 'Hello, KemoKemo!'
 
-def main():
+def main() -> None:
     args = sys.argv[1:]
-    name = args[0] if args else 'World'
-    print(greet(name))
+    if len(args) != 1:
+        prg_name = os.path.basename(sys.argv[0])
+        print(f'usage: {prg_name} NAME')
+        sys.exit(1)
+    else:
+        print(greet(args[0]))
 
 if __name__ == '__main__':
     main()
