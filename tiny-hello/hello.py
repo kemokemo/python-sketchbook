5796ef6f5b46c1adc72835ee21103bc507fb416e
kemokemo <t2wonderland@gmail.com>
2020-03-16 12:36:41 +0900

    docs: fixed by yapf -i command.

--- tiny-hello/hello.py
+++ tiny-hello/hello.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
+"""
+Author: kemokemo <t2wonderland@gmail.com>
+Purpose: Say hello
+"""
 
 import argparse
+
 
 def greet(name: str) -> str:
     return f'Hello, {name}!'
 
+
 def test_greet() -> None:
     assert greet('World') == 'Hello, World!'
     assert greet('KemoKemo') == 'Hello, KemoKemo!'
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('name', help='Name to greet')
     args = parser.parse_args()
     print(greet(args.name))
+
 
 if __name__ == '__main__':
     main()
