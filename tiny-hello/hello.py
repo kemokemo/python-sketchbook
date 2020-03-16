1b18f7adda0f55191cc543c6b252f8a48507f4f3
kemokemo <t2wonderland@gmail.com>
2020-03-16 12:41:20 +0900

    docs: add docstring to solve pylint's warn.

--- tiny-hello/hello.py
+++ tiny-hello/hello.py
@@ -6,17 +6,17 @@ Purpose: Say hello
 
 import argparse
 
-
 def greet(name: str) -> str:
+    """ greet returns a greeting message for 'name'. """
     return f'Hello, {name}!'
 
-
 def test_greet() -> None:
+    """ test for greet functionb. """
     assert greet('World') == 'Hello, World!'
     assert greet('KemoKemo') == 'Hello, KemoKemo!'
 
-
 def main() -> None:
+    """ endpoint of this app. """
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('name', help='Name to greet')
     args = parser.parse_args()
